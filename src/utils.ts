e9ef20de11ac8bbf0e5426e5b892da0aac9ac4c0
Oleg Valter <o.a.philippov@gmail.com>
2022-04-10 18:46:38 +0300

    added utility for formatting markdown links

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,10 @@ export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, 
  * @param err error to check
  */
 export const isErrno = (err: unknown): err is NodeJS.ErrnoException => err instanceof Error && "code" in err;
+
+/**
+ * @summary makes a markdown URL
+ * @param url URL to format
+ * @param label label to provide
+ */
+export const mdURL = (url: string, label: string) => `[${label}](${url})`;
