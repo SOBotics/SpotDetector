0c97f40fad4c5d00237f7ad54759e32817c6455b
Oleg Valter <o.a.philippov@gmail.com>
2022-04-10 18:11:14 +0300

    reworked timeline parsing

--- src/parsers/timeline.ts
+++ src/parsers/timeline.ts
@@ -1,142 +1,148 @@
 const DUPLICATE_REGEX = /meta\.stackexchange\.com\/q\/104227/i;
 
-export const parseTimeline = $ =>
-    $(".event-rows tr:not(.separator)")
-        .toArray()
-        .reduce(
-            (arr, row) => {
-                const id = $(row).data("eventid");
-                const eventtype = $(row).data("eventtype");
-                const verb = $(row)
-                    .find(".event-verb span")
-                    .text()
-                    .trim();
+export const parseTimeline = (doc: Document) => {
+    const rows = [...doc.querySelectorAll<HTMLTableRowElement>(".event-rows tr:not(.separator)")];
 
-                switch (eventtype) {
-                    case "history":
-                        switch (verb) {
-                            case "asked":
-                            case "answered":
-                                const userA = $(row).find(".created-by a");
-                                let userId;
+    const init: {
+        deleted: unknown | null,
+        deletedBy: string[],
+        deleteReason: string,
+        reviews: Record<string, {
+            eventid: string,
+            link: string,
+            result?: string,
+            type: string;
+        }>;
+    } = {
+        deleted: null,
+        deletedBy: [],
+        deleteReason: "",
+        reviews: {}
+    };
 
-                                if (userA.length > 0) {
-                                    userId = userA.attr("href").split("/")[2];
-                                } else {
-                                    userId = $(row)
-                                        .find(".created-by")
-                                        .text()
-                                        .trim();
-                                }
+    return rows.reduce(
+        (info, row) => {
+            const { dataset } = row;
 
-                                if (
-                                    arr.deleted &&
-                                    arr.deletedBy.includes(userId) &&
-                                    arr.deleteReason !== "duplicate"
-                                ) {
-                                    arr.deleteReason = userId.startsWith("user")
-                                        ? "self_nuked"
-                                        : "self";
-                                }
-                                break;
-                            case "post deleted from review":
-                                if (arr.deleted === null) {
-                                    arr.deleteReason = "review";
-                                    arr.deleted = true;
-                                }
-                                break;
-                            case "deleted":
-                                if (arr.deleted === null) {
-                                    arr.deleted = true;
+            const { eventid, eventtype } = dataset;
 
-                                    if (arr.deleteReason !== "duplicate") {
-                                        if ($(row).find(".created-by .mod-flair").length > 0) {
-                                            if (
-                                                $(row)
-                                                    .find(".event-comment span")
-                                                    .text()
-                                                    .trim() === "Converted to Comment"
-                                            ) {
-                                                arr.deleteReason = "diamond_mod_convert";
-                                            } else {
-                                                arr.deleteReason = "diamond_mod";
-                                            }
+            const verbElem = row.querySelector<HTMLSpanElement>(".event-verb span");
+
+            const verb = verbElem?.textContent || "";
+
+            switch (eventtype) {
+                case "history":
+                    switch (verb) {
+                        case "asked":
+                        case "answered":
+                            const userA = row.querySelector<HTMLAnchorElement>(".created-by a");
+
+                            let userId;
+
+                            if (userA) {
+                                userId = userA.href.split("/")[2];
+                            } else {
+                                userId = row.querySelector(".created-by")?.textContent?.trim();
+                            }
+
+                            if (!userId) break;
+
+                            if (
+                                info.deleted &&
+                                info.deletedBy.includes(userId) &&
+                                info.deleteReason !== "duplicate"
+                            ) {
+                                info.deleteReason = userId.startsWith("user")
+                                    ? "self_nuked"
+                                    : "self";
+                            }
+                            break;
+                        case "post deleted from review":
+                            if (info.deleted === null) {
+                                info.deleteReason = "review";
+                                info.deleted = true;
+                            }
+                            break;
+                        case "deleted":
+                            if (info.deleted === null) {
+                                info.deleted = true;
+
+                                if (info.deleteReason !== "duplicate") {
+                                    const flair = row.querySelector(".created-by .mod-flair");
+
+                                    if (flair) {
+                                        if (
+                                            row.querySelector(".event-comment span")?.textContent?.trim() === "Converted to Comment"
+                                        ) {
+                                            info.deleteReason = "diamond_mod_convert";
                                         } else {
-                                            arr.deleteReason = "reputation_mod";
+                                            info.deleteReason = "diamond_mod";
                                         }
+                                    } else {
+                                        info.deleteReason = "reputation_mod";
                                     }
-
-                                    const userAs = $(row).find(".created-by a");
-
-                                    arr.deletedBy = [
-                                        ...userAs
-                                            .map(
-                                                (_, el) =>
-                                                    $(el)
-                                                        .attr("href")
-                                                        .split("/")[2]
-                                            )
-                                            .toArray(),
-                                        ...(userAs.length
-                                            ? []
-                                            : [
-                                                $(row)
-                                                    .find(".created-by")
-                                                    .text()
-                                                    .trim()
-                                            ])
-                                    ];
                                 }
-                                break;
-                            case "undeleted":
-                                if (arr.deleted === null) {
-                                    arr.deleted = false;
-                                }
-                                break;
-                        }
-                        break;
-                    case "comment":
-                        const comment = $(row)
-                            .find(".event-comment span")
-                            .html();
-                        if (DUPLICATE_REGEX.test(comment)) {
-                            arr.deleteReason = "duplicate";
-                        }
-                    case "review":
-                        if ($(row).hasClass("deleted-event")) {
-                            const a = $(row).find(".event-verb span a");
-                            const link = a.attr("href");
-                            const reviewType = a.text().trim();
 
+                                const userAs = [...row.querySelectorAll<HTMLAnchorElement>(".created-by a")];
+
+                                info.deletedBy = [
+                                    ...userAs.map((el) => el.href.split("/")[2]),
+                                    ...(userAs.length
+                                        ? []
+                                        : [row.querySelector(".created-by")?.textContent?.trim() || ""])
+                                ];
+                            }
+                            break;
+                        case "undeleted":
+                            if (info.deleted === null) {
+                                info.deleted = false;
+                            }
+                            break;
+                    }
+                    break;
+                case "comment":
+                    const commentElem = row.querySelector(".event-comment span");
+
+                    const comment = commentElem?.innerHTML || "";
+
+                    if (DUPLICATE_REGEX.test(comment)) {
+                        info.deleteReason = "duplicate";
+                    }
+                case "review":
+                    if (row.classList.contains("deleted-event")) {
+                        const a = row.querySelector<HTMLAnchorElement>(".event-verb span a");
+
+                        const link = a?.href || "";
+                        const reviewType = a?.textContent?.trim();
+
+                        if (eventid) {
                             switch (reviewType) {
                                 case "late answer":
                                 case "first post":
-                                    arr.reviews[id] = {
-                                        id,
+                                    info.reviews[eventid] = {
+                                        eventid,
                                         link,
                                         type: reviewType
                                     };
                                     break;
                             }
-                        } else if (
-                            $(row).hasClass("deleted-event-details") &&
-                            typeof arr.reviews[id] !== "undefined"
-                        ) {
-                            const comment = $(row)
-                                .find(".event-comment span")
-                                .text()
-                                .trim();
-
-                            arr.reviews[id].result = comment.replace(/ × 1$/, "");
                         }
-                        break;
-                }
+                    } else if (
+                        row.classList.contains("deleted-event-details") &&
+                        eventid &&
+                        typeof info.reviews[eventid] !== "undefined"
+                    ) {
+                        const commentElem = row.querySelector<HTMLSpanElement>(".event-comment span");
 
-                return arr;
-            },
-            {
-                deleted: null,
-                deletedBy: [],
-                reviews: {}
+                        const comment = commentElem?.textContent?.trim() || "";
+
+                        info.reviews[eventid].result = comment.replace(/ × 1$/, "");
+                    }
+                    break;
             }
-        );
+
+            return info;
+        },
+        init
+    );
+};
