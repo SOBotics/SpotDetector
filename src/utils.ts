06c08ef4f301b47ee668ee8cafae2c462d3a47a3
Oleg Valter <o.a.philippov@gmail.com>
2022-04-10 17:45:58 +0300

    added utility guard for checking if an error is a Node.js error

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,9 @@
  * @param ms milliseconds to delay for
  */
 export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * @summary checks if an error is a Node.js error
+ * @param err error to check
+ */
+export const isErrno = (err: unknown): err is NodeJS.ErrnoException => err instanceof Error && "code" in err;
