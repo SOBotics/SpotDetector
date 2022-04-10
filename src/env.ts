b92bf92e555d005c0736f28d1640395bd4fdc5b9
Oleg Valter <o.a.philippov@gmail.com>
2022-04-10 18:24:53 +0300

    properly typed env interface for the bot

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,16 @@
 import dotenv from "dotenv";
 
+export interface BotEnvironment {
+    API_KEY: string;
+    CHAT_EMAIL: string;
+    CHAT_PASSWORD: string;
+    CHAT_ROOM: string;
+    TENK_EMAIL: string;
+    TENK_PASSWORD: string;
+}
+
 const { parsed } = dotenv.config();
 
-export default parsed;
+const environment: Partial<BotEnvironment> = parsed || {};
+
+export default environment;
