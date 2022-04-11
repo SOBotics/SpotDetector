e385b32ff416dfff467a0e23bdc7a3cecdf827e1
Oleg Valter <o.a.philippov@gmail.com>
2022-04-11 12:49:23 +0300

    added unit testing boilerplate for timeline parsers

---
+++ test/unit/parsers.spec.ts
@@ -0,0 +1,35 @@
+describe("Timeline Parsers", () => {
+
+    describe.skip("Close Votes", () => {
+
+    });
+
+    describe.skip("First Answers", () => {
+
+    });
+
+    describe.skip("First Questions", () => {
+
+    });
+
+    describe.skip("Late Answers", () => {
+
+    });
+
+    describe.skip("Low-Quality Posts", () => {
+
+    });
+
+    describe.skip("Reopen Votes", () => {
+
+    });
+
+    describe.skip("Suggested Edits", () => {
+
+    });
+
+    describe.skip("Triage", () => {
+
+    });
+
+});
