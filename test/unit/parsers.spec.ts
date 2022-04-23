8389dc0f34806e34f701d69d14d34f17451a12f1
Oleg Valter <o.a.philippov@gmail.com>
2022-04-23 20:50:49 +0300

    added unit tests for deletion event parsing

--- test/unit/parsers.spec.ts
+++ test/unit/parsers.spec.ts
@@ -9,6 +9,47 @@ describe("Timeline Parsers", () => {
     const postMock = readFileSync("test/mocks/timeline.html", { encoding: "utf-8" });
     const { window: { document } } = new JSDOM(postMock);
 
+    describe('Deletions', () => {
+        const { deletions } = parseTimeline(document);
+
+        const events = Object.values(deletions);
+
+        it('should correctly parse duplicate deletions', () => {
+            const dupe = events.find(({ reason }) => reason === "duplicate");
+            expect(dupe).to.not.be.undefined;
+
+            if (dupe) {
+                const { by } = dupe;
+                expect(by.length).to.equal(1);
+            }
+        });
+
+        it('should correctly parse review deletions', () => {
+            const review = events.find(({ reason }) => reason === "review");
+            expect(review).to.not.be.undefined;
+
+            if (review) {
+                const { by } = review;
+                expect(by.length).to.equal(4);
+            }
+        });
+
+        it('should correctly parse normal deletions', () => {
+            const normal = events.find(({ reason }) => reason === "reputation_mod");
+            expect(normal).to.not.be.undefined;
+
+            if (normal) {
+                const { by } = normal;
+                expect(by.length).to.equal(3);
+            }
+        });
+
+        it('should correctly parse self deletions', () => {
+            const self = events.filter(({ reason }) => reason === "self");
+            expect(self.length).to.equal(3);
+        });
+    });
+
     describe.skip("Close Votes", () => {
 
     });
