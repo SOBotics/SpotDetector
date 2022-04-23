3219acf0ab667c31de76f3dec44834cbd02272d1
Oleg Valter <o.a.philippov@gmail.com>
2022-04-23 20:11:05 +0300

    added unit test for parsing suggested edit reviews from post timeline

--- test/unit/parsers.spec.ts
+++ test/unit/parsers.spec.ts
@@ -1,4 +1,13 @@
+import { expect } from "chai";
+import { readFileSync } from "fs";
+import { JSDOM } from "jsdom";
+import { ReviewType } from "../../src/fetchers/reviews.js";
+import { parseTimeline } from "../../src/parsers/timeline.js";
+
 describe("Timeline Parsers", () => {
+
+    const postMock = readFileSync("test/mocks/timeline.html", { encoding: "utf-8" });
+    const { window: { document } } = new JSDOM(postMock);
 
     describe.skip("Close Votes", () => {
 
@@ -24,8 +33,20 @@ describe("Timeline Parsers", () => {
 
     });
 
-    describe.skip("Suggested Edits", () => {
+    describe("Suggested Edits", () => {
+        it('should correctly parse suggested edits review events', () => {
+            const { reviews } = parseTimeline(document);
 
+            const suggestion = Object.values(reviews).find(({ type }) => type === ReviewType.SE);
+
+            expect(suggestion).to.not.be.undefined;
+
+            if (suggestion) {
+                const { result, votes } = suggestion;
+                expect(result).to.equal("completed");
+                expect(votes.approve).to.equal(1);
+            }
+        });
     });
 
     describe.skip("Triage", () => {
