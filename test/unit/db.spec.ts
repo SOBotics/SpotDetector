b5ccd8175ba859bfe12c5463968d075cc400097e
Oleg Valter <o.a.philippov@gmail.com>
2022-04-24 06:34:54 +0300

    added unit tests for the addReview and addPost DB utilities

--- test/unit/db.spec.ts
+++ test/unit/db.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
-import { createPostsTable, createReviewsTable, openDatabase } from "../../src/db.js";
+import { addPost, addReview, createPostsTable, createReviewsTable, openDatabase, PostFromDB, ReviewFromDB } from "../../src/db.js";
+import { PostType, ReviewType } from "../../src/fetchers/index.js";
 
 describe('Database', () => {
     describe(createPostsTable.name, () => {
@@ -56,6 +57,57 @@ describe('Database', () => {
 
             expect(result.type).to.equal("TEXT");
             expect(result.notnull).to.equal(1);
+        });
+    });
+
+    describe(addPost.name, () => {
+        it('should correctly add posts', async () => {
+            const db = await openDatabase(":memory:");
+
+            await createPostsTable(db);
+
+            await addPost(db, 42, PostType.A);
+            await addPost(db, 9000, PostType.Q);
+
+            const posts: PostFromDB[] = await db.all("SELECT * FROM posts WHERE id in (42,9000)");
+
+            const [answer, question] = posts;
+
+            expect(answer.id).to.equal(42);
+            expect(question.id).to.equal(9000);
+
+            expect(answer.type).to.equal(PostType.A);
+            expect(question.type).to.equal(PostType.Q);
+        });
+    });
+
+    describe(addReview.name, () => {
+        it('should correctly add reviews', async () => {
+            const db = await openDatabase(":memory:");
+
+            await createReviewsTable(db);
+
+            const now = Math.trunc(Date.now() / 1000);
+
+            await addReview(db, "42", ReviewType.SE, {
+                action: "approve",
+                date: now,
+                post_id: 9000,
+                user_id: "-1",
+                user_name: "Community"
+            });
+
+            const reviews: ReviewFromDB[] = await db.all("SELECT * FROM reviews WHERE review_id = 42");
+
+            const [{ user_id, user_name, post_id, review_id, review_result, review_type, date }] = reviews;
+
+            expect(review_id).to.equal(42);
+            expect(review_type).to.equal(ReviewType.SE);
+            expect(post_id).to.equal(9000);
+            expect(user_id).to.equal(-1);
+            expect(user_name).to.equal("Community");
+            expect(date).to.equal(now);
+            expect(review_result).to.equal("approve");
         });
     });
 });
