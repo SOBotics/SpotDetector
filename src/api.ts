ed4460b2b44f129a53c15dfffa20ea3573179bc0
Oleg Valter <o.a.philippov@gmail.com>
2022-05-02 11:40:45 +0300

    added SE API utility for getting suggested edits

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
-import type { Privilege, User, Wrappers } from "@userscripters/stackexchange-api-types";
+import type { Privilege, SuggestedEdit, User, Wrappers } from "@userscripters/stackexchange-api-types";
 import request from "request-promise-native";
-import { delay } from "./utils.js";
+import { delay, getSeconds } from "./utils.js";
 
 export interface CommonGetOptions {
     filter?: string;
@@ -15,9 +15,16 @@ export interface CommonPerSiteOptions extends CommonGetOptions {
     site?: string;
 }
 
+export interface CommonIntervaledOptions extends CommonGetOptions {
+    from?: string | Date | number;
+    to?: string | Date | number;
+}
+
 export interface GetPrivilegesOptions extends CommonPaginatedOptions, CommonPerSiteOptions { }
 
 export interface GetUserInfoOptions extends CommonPaginatedOptions, CommonPerSiteOptions { }
+
+export interface GetSuggestedEditsOptions extends CommonPaginatedOptions, CommonIntervaledOptions, CommonPerSiteOptions { }
 
 const state = {
     backoff: 0
@@ -174,4 +181,62 @@ export const getUserInfo = async <T extends keyof User>(
 
     items.forEach((item) => users.set(item[keyOn], item));
     return users;
+};
+
+/**
+ * {@see https://api.stackexchange.com/docs/suggested-edits}
+ * @param keyOn key to use for the {@link Map}
+ * @param options request configuration
+ */
+export const getSuggestedEdits = async <T extends keyof SuggestedEdit>(
+    type: "all" | "approved" | "rejected",
+    keyOn: T,
+    options: GetSuggestedEditsOptions
+): Promise<Map<SuggestedEdit[T], SuggestedEdit>> => {
+    await delay(state.backoff);
+
+    const { page = 1, site = "stackoverflow", from, to, ...rest } = options;
+
+    const params = new URLSearchParams({
+        ...rest,
+        page: page.toString(),
+        pagesize: "100",
+        site
+    });
+
+    if (type !== "all") {
+        params.set("sort", type === "approved" ? "approval" : "rejection");
+    }
+
+    if (from) params.set("fromdate", getSeconds(from).toString());
+    if (to) params.set("todate", getSeconds(to).toString());
+
+    const url = new URL(`${API_BASE}/${API_VER}/suggested-edits`);
+    url.search = params.toString();
+
+    const res: Wrappers.CommonWrapperObject<SuggestedEdit> = await request(url.toString(), {
+        gzip: true,
+        json: true
+    });
+
+    const { backoff, has_more, items = [] } = res;
+
+    if (backoff) {
+        state.backoff = backoff * 1e3 + 5;
+        return getSuggestedEdits(type, keyOn, options);
+    }
+
+    const edits = new Map<SuggestedEdit[T], SuggestedEdit>();
+
+    if (has_more) {
+        const more = await getSuggestedEdits(type, keyOn, {
+            ...options,
+            page: page + 1
+        });
+
+        more.forEach((v, k) => edits.set(k, v));
+    }
+
+    items.forEach((item) => edits.set(item[keyOn], item));
+    return edits;
 };
